68c5caebbaff889a2e3a418d4df845fe4e59104a
pkdc <pkdchoy@gmail.com>
2020-10-28 13:55:15 +0000

    Trying to leave the punctuation marks untouched, but failed. Can either leave the whole word with the punct untoched, or making changes to the word and the pucnt

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -6,18 +6,22 @@ function pigLatin(str){
     // let str_arr_space = str.split(' ');
     // let str_space = str_arr_space.toString();
     // let str_arr = str_space.split(/\W/);
-    // let str_arr = .split(/\W/);
+
+    // let str_arr = str.split(/\W/);
 
     let str_arr = str.split(" ");
-    console.log(str_arr);
+    // console.log(str_arr);
 
     // (ignoring punctuation) use RegEx (Not solved!!!!!!!) (search for \W, take them out, and put them back?)
     // get the first letter of each words with substr
     // put it to the end of it with
     // add "ay" to the end with
-    let re = /\w/;
+    // let re = /\w/;
     var pigLatinStrArr = str_arr.map(el => {
-        if(re.test(el)) {
+        // if(re.test(el)) {    //wrong, the punctuation will be affected
+    let re = /[,!@#$%^&*()]/;   //wrong, the whole word would be ignored
+        // if(re.test(el)) {
+        if(!re.test(el)) {
             return el.substr(1, el.length - 1) + el.substr(0,1) + "ay";
         }
         else {
@@ -25,7 +29,7 @@ function pigLatin(str){
         }
     });
 
-    console.log(pigLatinStrArr);
+    // console.log(pigLatinStrArr);
     // sticking them back tgt
     let pigLatinStr = "";
     for (const cur of pigLatinStrArr) {
@@ -36,7 +40,10 @@ function pigLatin(str){
     return pigLatinStr;
 }
 
-pigLatin('Hello, My name ^;^ is Dave!! Yo');
+pigLatin('Pig latin is cool');
+pigLatin('This is my string');
+pigLatin('Pig latin is cool!!');
+
 // module.exports = {
 //     pigLatin
 // };
